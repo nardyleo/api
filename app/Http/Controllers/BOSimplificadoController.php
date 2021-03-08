ebed5bdb61472f59055418c57bec55ca336374fc
Leonardo Nardy <nardyleo@hotmail.com>
2021-03-08 16:57:19 -0300

    Adicionando request para validação

--- app/Http/Controllers/BOSimplificadoController.php
+++ app/Http/Controllers/BOSimplificadoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\BOSimplificadoRequest as Request;
 use App\FormularioBOS;
 
 class BOSimplificadoController extends Controller
